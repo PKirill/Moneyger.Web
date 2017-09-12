8067fc6ba2d6c691e4317e114005467f61676135
Kyrylo Polishchuk <polishyk007@gmail.com>
2017-09-12 21:17:49 +0300

    Add new class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+    protected testVar: number;
+
+    protected FirstLevel() {
+        var a = 5;
+        this.testVar = 6;
+
+        function SecondLevel() {
+            var a = 6;
+
+            function DeepLevel() {
+                console.log(a);
+                this.testVar = "sum dummy";
+                console.log(this.testVar);
+            }
+
+            DeepLevel();
+            console.log(this.testVar);
+        }
+
+        SecondLevel();
+        console.log(this.testVar);
+    }
+
+    ngAfterViewInit() {
+        this.FirstLevel();
+    }
+
   title = 'app';
 }
